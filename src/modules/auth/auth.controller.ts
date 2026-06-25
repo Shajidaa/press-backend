16f41fc6ccf2e20fdcc2f8d27463711f9217bd8e
Shajidaa <shajidaakterlopa101@gmail.com>
2026-06-25 09:53:09 +0600

    feat(auth):cookie utils function implement

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,15 +3,26 @@ import { catchAsync } from "../../utils/catchAsync";
 import { authService } from "./auth.service";
 import sendResponse from "../../utils/sendResponse";
 import httpStatus from "http-status";
+import setCookie from "../../utils/cookie";
 const login = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-    const result = await authService.loginFromDB(payload);
+    const { accessToken, refreshToken } =
+      await authService.loginFromDB(payload);
+    // tokens set on browser
+    setCookie(res, "accessToken", accessToken, 60 * 60 * 24);
+
+    setCookie(
+      res,
+      "refreshToken",
+      refreshToken,
+      Number(process.env.COOKIE_REFRESH_MAX_AGE),
+    );
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
       message: "User login successfully",
-      data: result,
+      data: { accessToken, refreshToken },
     });
   },
 );
