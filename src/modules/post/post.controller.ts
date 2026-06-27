02fd96ec8315cee7501825a3359487f6bb1f408a
Shajidaa <shajidaakterlopa101@gmail.com>
2026-06-27 09:40:41 +0600

    feat(post):impelement created and all post get api

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,10 +1,30 @@
 import { catchAsync } from "../../utils/catchAsync";
 import type { Request, Response, NextFunction } from "express";
+import { postService } from "./post.service";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
 const createdPost = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.user?.id;
+    const result = await postService.createdPostDB(req.body, id as string);
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Post created successfully!",
+      data: result,
+    });
+  },
 );
 const getAllPost = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await postService.getAllPostDB();
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "get All post successfully!",
+      data: result,
+    });
+  },
 );
 const getPostStats = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {},
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,5 +1,26 @@
-const createdPostDB = {};
-const getAllPostDB = {};
+import { prisma } from "../../lib/prisma";
+import { ICreatedPostPayload } from "./post.interface";
+
+const createdPostDB = async (payload: ICreatedPostPayload, userId: string) => {
+  const post = await prisma.post.create({
+    data: {
+      ...payload,
+      authorId: userId,
+    },
+  });
+  return post;
+};
+const getAllPostDB = async () => {
+  const allPost = await prisma.post.findMany({
+    include: {
+      author: {
+        omit: { password: true },
+      },
+      comments: true,
+    },
+  });
+  return allPost;
+};
 const getPostStatsDB = {};
 const getMyPostsDB = {};
 const getMyPostByIdDB = {};
